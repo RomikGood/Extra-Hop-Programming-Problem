32a39928c188a13ecd3bb37b4acf4155e2d4a006
Roman Kireev <kireev@gmail.com>
2018-12-26 18:23:20 -0800

    added to main function

--- longest_word.py
+++ longest_word.py
@@ -2,47 +2,44 @@ import html_scraping
 
 def find_longest_word(grid, words):
 
-
-    """
-    Recursive function to verify that the candidate word exists in the grid
-    and if it does, funcion returns the word's length
-    """
-    def if_word_exists(grid, word, r, c, index):
-        word = word.upper()
-        
+    def if_word_exists(grid, word, row, col, index):
+        """
+        Recursive function to verify that the candidate word exists in the grid
+        """
+        word = word.upper()        
         # Valid chess knight moves 
         moves = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
-        if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
             return False
         if index == len(word):
-            return len(word)
+            return True
+
         for x, y in moves:
-            letter = grid[r][c]
-            if letter == word[index] and if_word_exists(grid, word, r+x, c+y, index+1):
+            letter = grid[row][col]
+            # print(f'letter: {letter}, word index: {word[index]}, index: {index}, row: {row}, col: {col}')
+            if letter != word[index]:
+                return False
+
+            if letter == word[index] and if_word_exists(grid, word, row+x, col+y, index+1):
                 return True
-    
+
+
     # Verifing that grid and list of word exist
     if not grid and words or grid and not words or not grid and not words:
-        return 'Please provide valid grid and list of words'
+        return 'Please provide valid grid and/or list of words'
 
     max_length = 0
     try:
         for word in words:
-            for r in range(len(grid)):
-                for c in range(len(grid[0])):
-                    if if_word_exists(grid, word, r, c, 0):
+            for row in range(len(grid)):
+                for col in range(len(grid[0])):
+                    if if_word_exists(grid, word, row, col, 0):
                         if len(word) > max_length:
                             max_length = len(word)
                             longest_word = word
         return longest_word
     except:
         return 'No words could be produced by this Grid'
-
-# words = ['forthhh', 'fortranhh', 'fortranhhh']
-# words = []
-# words = Html_scraping.word_list
-
-# grid3 = []
 
 # grid = [['Q', 'W', 'E', 'R', 'T', 'N', 'U', 'I'],
 #         ['O', 'P', 'A', 'A', 'D', 'F', 'G', 'H'],
@@ -52,18 +49,7 @@ def find_longest_word(grid, words):
 #         ['G', 'H', 'J', 'O', 'L', 'Z', 'X', 'C'],
 #         ['V', 'B', 'N', 'M', 'Q', 'W', 'E', 'R'],
 #         ['T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S']]
+	
+# words = ['wlph']
 
-
-
-# grid2 = [['E', 'X', 'T', 'R', 'A', 'H', 'O', 'P'],
-#         ['N', 'E', 'T', 'W', 'O', 'R', 'K', 'S'],
-#         ['Q', 'I', 'H', 'A', 'C', 'I', 'Q', 'T'],
-#         ['L', 'F', 'U', 'N', 'U', 'R', 'X', 'B'],
-#         ['B', 'W', 'D', 'I', 'L', 'A', 'T', 'V'],
-#         ['O', 'S', 'S', 'Y', 'N', 'A', 'C', 'K'],
-#         ['Q', 'W', 'O', 'P', 'M', 'T', 'C', 'P'],
-#         ['K', 'I', 'P', 'A', 'C', 'K', 'E', 'T']]
-
-
-# print(find_longest_word(grid2, words))
-
+# print(find_longest_word(grid, words))
--- test_logest_word.py
+++ test_logest_word.py
@@ -4,20 +4,26 @@ import html_scraping
 
 
 def test_find_longest_word_exists():
-		"""Test verifies that find longest word fanction exsits
-		"""
-		assert find_longest_word
-
-
-def test_if_no_grid_or_no_words():
-	"""test returns if no grid or no word list is provided
 	"""
-	assert find_longest_word([], []) == 'Please provide valid grid and list of words'
-	assert find_longest_word(['A', 'B', 'C'], []) == 'Please provide valid grid and list of words'
-	assert find_longest_word([], ['extrahop', 'TCP']) == 'Please provide valid grid and list of words'
+	Test verifies that find longest word fanction exsits
+	"""
+	assert find_longest_word
+
+
+def test_if_no_grid_or_no_words_provided():
+	"""
+	test returns if no grid or no word list is provided
+	"""
+	assert find_longest_word([], []) == 'Please provide valid grid and/or list of words'
+	assert find_longest_word(['A', 'B', 'C'], []) == 'Please provide valid grid and/or list of words'
+	assert find_longest_word([], ['extrahop', 'TCP']) == 'Please provide valid grid and/or list of words'
 
 
 def test_with_8by8_grid_and_word_list():
+	"""
+	test if function return the longest word from a list 
+	if multiple words can be produced
+	"""
 	grid = [['Q', 'W', 'E', 'R', 'T', 'N', 'U', 'I'],
         ['O', 'P', 'A', 'A', 'D', 'F', 'G', 'H'],
         ['T', 'K', 'L', 'Z', 'X', 'C', 'V', 'B'],
@@ -27,12 +33,13 @@ def test_with_8by8_grid_and_word_list():
         ['V', 'B', 'N', 'M', 'Q', 'W', 'E', 'R'],
         ['T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S']]
 	
-	words = ['algol', 'fortran', 'simula']
+	words = ['algol', 'fortran', 'fort']
 	assert find_longest_word(grid, words) == 'fortran'
 
 
 def test_with_no_words_found_in_grid():
-	"""Test function return if no words for a list can be produced by the grid
+	"""
+	Test function return if no words for a list can be produced by the grid
 	"""
 	grid = [['Q', 'W', 'E', 'R', 'T', 'N', 'U', 'I'],
         ['O', 'P', 'A', 'A', 'D', 'F', 'G', 'H'],
@@ -43,7 +50,7 @@ def test_with_no_words_found_in_grid():
         ['V', 'B', 'N', 'M', 'Q', 'W', 'E', 'R'],
         ['T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S']]
 	
-	words = ['algolss', 'fortranss', 'simulass']
+	words = ['algols', 'fortranss', 'simulass']
 	assert find_longest_word(grid, words) == 'No words could be produced by this Grid'
 
 
